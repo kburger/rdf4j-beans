813d1a018460d49586788779c3d79b03f6fe5b9b
kburger <burger.github@gmail.com>
2017-03-15 19:45:11 +0100

    Updated beanmapper javadoc to describe the read functionality

--- src/main/java/com/github/kburger/rdf4j/beans/BeanMapper.java
+++ src/main/java/com/github/kburger/rdf4j/beans/BeanMapper.java
@@ -21,15 +21,15 @@ import java.io.Writer;
 import org.eclipse.rdf4j.rio.RDFFormat;
 
 /**
- * Bean mapper that ties the {@link BeanAnalyzer} and {@link BeanWriter} classes together. This
- * class is the main usage entry point. Behaviour can be modified by setting a custom {@code
- * BeanWriter}.
+ * Bean mapper that ties the {@link BeanAnalyzer}, {@link BeanWriter}, and {@link BeanReader}
+ * classes together. This class is the main usage entry point. Behaviour can be modified by setting
+ * a custom {@code BeanWriter} and/or {@code BeanReader}.
  * 
  * <p>Usage example:
  * <pre>
  *    // bean usage example class
  *    &#64;Type("http://example.com/Type")
- *    public class ExampleBean {
+ *    public class StringExampleBean {
  *        &#64;Predicate("http://purl.org/dc/terms/creator")
  *        private URI creator;
  *        &#64;Predicate(value = "http://purl.org/dc/terms/title", isLiteral = true)
@@ -46,12 +46,18 @@ import org.eclipse.rdf4j.rio.RDFFormat;
  *    
  *    // mapper usage example
  *    BeanMapper mapper = new BeanMapper();
- *    mapper.write(new StringWriter(), new ExampleBean(), "http://example.com/subject", RDFFormat.TURTLE);
+ *    mapper.write(new StringWriter(), new StringExampleBean(), "http://example.com/subject", RDFFormat.TURTLE);
  *    
  *    // stringwriter output now holds:
  *    &lt;http://example.com/subject&gt; a &lt;http://example.com/Type&gt; ;
  *        &lt;http://purl.org/dc/terms/creator&gt; &lt;...&gt; ;
  *        &lt;http://purl.org/dc/terms/title&gt; "..." .
+ * </pre>
+ * 
+ * <p>Given the previous RDF output, it can be converted back into a Java bean using the following
+ * snippet:
+ * <pre>
+ *    StringExampleBean bean = mapper.read(new StringReader(content), StringExampleBean.class, RDFFormat.TURTLE);
  * </pre>
  */
 public class BeanMapper {
@@ -68,12 +74,13 @@ public class BeanMapper {
     }
     
     /**
-     * Writes a java bean, annotated with the rdf4j-beans annotations, to the writer using the
-     * given format.
+     * Writes a Java bean, annotated with the rdf4j-beans annotations, to the {@code writer} using
+     * the given {@code format}.
      * @param writer {@link Writer} used to output the serialized bean.
      * @param bean java bean to serialze.
      * @param subject triple subject for the java bean.
      * @param format determines the serialization format.
+     * @throws BeanException if an error occurred during serialization.
      */
     public void write(Writer writer, Object bean, String subject, RDFFormat format) {
         final Class<?> clazz = bean.getClass();
@@ -81,6 +88,15 @@ public class BeanMapper {
         beanWriter.write(writer, analysis, bean, subject, format);
     }
     
+    /**
+     * Reads a Java bean from the RDF content provided by the {@code reader} in the given {@code
+     * format}.
+     * @param reader {@link Reader} instance that provides the RDF content.
+     * @param clazz target bean class.
+     * @param format RDF content format.
+     * @return a populated bean instance of type {@code clazz}.
+     * @throws BeanException if an error occured during deserialization.
+     */
     public <T> T read(Reader reader, Class<T> clazz, RDFFormat format) {
         final ClassAnalysis analysis = analyzer.analyze(clazz);
         return beanReader.read(reader, clazz, analysis, format);
